15ff526900949f3ed583c43f2722ba59870325fe
binjy <72844741+binjy@users.noreply.github.com>
2021-03-11 11:01:22 -0700

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,29 @@
+
+Function preload() {
+ 
+  
+  
+}
+  
+
+
+
+
+
+Function setup(){
+createCanvas(1000,1000)
+
+
+}
+
+
+Function draw() {
+  background("Red")
+  
+  
+  drawSprites()
+}
+
+
+
+
